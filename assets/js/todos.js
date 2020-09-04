6152a05d86b041a9e4377d914831490e0dc604d0
DeepAntala <deep.antala@yahoo.com>
2020-09-04 23:12:09 +0400

    Update todos.js

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,17 +1,22 @@
+
+
+document.addEventListener('DOMContentLoaded', getList);
+
 $("ul").on("click", "li", function(){
 	$(this).toggleClass("checkout");
-			
 });
 
-$("button").on("click",function(){
+$("button").on("click",function(event){
+	event.preventDefault();
 	var enterText=$("input[type='text']").val();
 		$("input[type='text']").val("");
 		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + enterText + "</li>");	
+		saveNewTodos(enterText);
 });
 
-$("ul").on("click", "span", function(event){
+$("ul").on("click", "span", function(){
 	$(this).parent().fadeOut(400, function(){
-		$(this).remove();
+	$(this).remove();
 	});
 	event.stopPropagation();
 })
@@ -26,4 +31,46 @@ $("input[type='text']").keypress(function(event){
 
 $(".fa-plus").click(function(){
 	$("input[type='text']").fadeToggle();
+	$("button").fadeToggle();
 })
+
+function saveNewTodos(item){
+	let items;
+		if(localStorage.getItem('items') === null){
+			items = [];
+		}
+		else{
+			items = JSON.parse(localStorage.getItem('items'));
+		}
+		items.push(item);
+		localStorage.setItem('items', JSON.stringify(items));
+
+		
+}
+
+function getList(){
+	let items;
+		if(localStorage.getItem('items') === null){
+			items = [];
+		}
+		else{
+			items = JSON.parse(localStorage.getItem('items'));
+		}
+	items.forEach(function(item){
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + item + "</li>");
+	})
+
+}
+
+function deleteItem(){
+	let items;
+		if(localStorage.getItem('items') === null){
+			items = [];
+		}
+		else{
+			items = JSON.parse(localStorage.getItem('items'));
+		}
+		$(this).pop();
+		localStorage.setItem('items', JSON.stringify(items));
+
+}
